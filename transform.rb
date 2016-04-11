1f0177ff913c385b376a262b4800a542bb0e4ea0
Dean Silfen <dean.silfen@gmail.com>
2016-04-10 23:13:39 -0400

    Update transform script to be useful

--- transform.rb
+++ transform.rb
@@ -1,31 +1,60 @@
 require 'json'
 require 'yaml'
+require 'rspotify'
+require 'time'
 
-posts = {}
-
-data = JSON.load(File.open('./posts.json').read)
-
-def to_markdown(filename, front_matter_hash, body)
-  front_matter = YAML.dump(front_matter_hash)
-  front_matter += "---\n"
-  body = front_matter + body + "\n"
-  File.open('./out/' + filename, 'w') { |f| f.write(body) }
-end
-
-data.each do |post|
-
-  if post['message'].nil?
-    puts "#{post['id']} has no message"
-    next
+class Album
+  attr_accessor :date, :artist, :album
+  def initialize(date, artist, album)
+    @date = Time.strptime(date, "%m/%d/%y").strftime('%y-%m-%d')
+    @artist = artist
+    @album = album
+    puts date, artist, album
   end
 
-  front_matter_hash = { 'layout' => 'post' }
-  ymd = post['created_time'][0..9]
-  poster = post['from']['name']
-  filename = "#{ymd}-#{poster.split.join('-')}.md"
-  front_matter_hash['title'] = "#{ymd} #{poster}"
-  front_matter_hash['comments'] = true
-  front_matter_hash['categories'] = poster.split.last
-  to_markdown(filename, front_matter_hash, post['message'])
+  def post
+    spotify_record = RSpotify::Album.search(album, market: 'US').first
+    if spotify_record.nil?
+      puts  "Not found #{album}"
+      return
+    end
+    front_matter_hash = {
+      'layout' => 'post',
+      'comments' => true,
+      'title' => post_title,
+    }
+    unless spotify_record.images.empty?
+      image = spotify_record.images.max{ |h| h['height'] }
+      front_matter_hash['cover_url'] = image['url']
+      front_matter_hash['cover_width'] = image['width']
+      front_matter_hash['cover_height'] = image['height']
+    end
+
+    yaml_front = YAML.dump(front_matter_hash) + "---\n"
+    body = spotify_record.embed + "\n"
+    File.open('./out/' + filename, 'w') { |f| f.write(yaml_front + body) }
+    puts "wrote file #{filename}"
+  end
+
+  def post_title
+    "#{date} #{artist}"
+  end
+
+  def filename
+    "#{@date}-#{artist.split.join('-')}-#{album.split.join('-')}.md"
+  end
+end
+
+data = JSON.load(File.open('./posts.json').read)
+albums = []
+File.open('./albums.txt').read.split("\n").delete_if { |i| i == '' }.each_slice(2) do |records|
+  title, album = records
+  puts title, album
+  date = title.split('-')[0].strip
+  artist = album.split('-')[0].strip
+  album = album.split('-')[-1].strip
+  puts "working on #{album}"
+  album_obj = Album.new(date, artist, album)
+  album_obj.post
 end
 
